5852cb9e699272c724c3bf8779b01aac1611f89c
Philip Drivenes <philipdrivenes@gmail.com>
2026-03-31 14:00:40 +0200

    utseende omoss

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -9,6 +9,8 @@ export default function AboutSection() {
         boxShadow: "0 -2px 15px rgba(0,0,0,0.1)",
         padding: "80px 40px",
         marginTop: "80px",
+        marginRight: "5%",
+        marginLeft: "5%",
       }}
     >
       <div className="max-w-4xl mx-auto">
@@ -19,6 +21,8 @@ export default function AboutSection() {
             color: "#000",
             fontWeight: 400,
             fontSize: "clamp(7px, 3vw, 60px)",
+            paddingRight: "10%",
+            paddingLeft: "10%",
           }}
         >
           Om Dus Arkitekter
@@ -30,14 +34,15 @@ export default function AboutSection() {
               style={{
                 color: "#434343",
                 fontWeight: 300,
-                fontSize: "clamp(6px, 1vw, 15px)",
+                fontSize: "clamp(10px, 2vw, 15px)",
                 lineHeight: "1.8",
+                paddingRight: "10%",
+                paddingLeft: "10%",
               }}
             >
               Vi er et lite og nyoppstartet, men ambisiøst arkitektkontor som er
               drevet av to engasjerte arkitekter - Synne Spjeld Høyvik og
               Anniken Marie Haugan.
-              <br />
               <br />
               Navnet Dus Arkitekter kommer fra vår visjon om å være
               &quot;dus&quot; med våre klienter, prosjekter og omgivelsene selv.
@@ -45,21 +50,27 @@ export default function AboutSection() {
               har blitt grunnleggende for vår estetikk som ellers er preget av
               renhet, funksjonalitet og tidløs eleganse.
               <br />
-              <br />
               Vi har erfaring innenfor tilbygg og transformasjon av hytter og
               eneboliger, men vårt interessefelt er bredt og vi tar gjerne både
               større og mindre prosjekter. Så hvis vi skulle være av interesse,
               ikke nøl med å ta kontakt for en hyggelig, uforpliktende prat.
             </p>
           </div>
-          <div className="flex flex-row gap-8 flex-wrap">
+          <div
+            className="flex flex-row gap-8 flex-wrap"
+            style={{
+              justifyContent: "center",
+              flexDirection: "row",
+              gap: "2rem",
+            }}
+          >
             {/* eslint-disable-next-line @next/next/no-img-element */}
             <img
               src="/img/oss.jpg"
               alt="Oss bak Dus!"
               style={{
                 boxShadow: "0 4px 10px rgba(0,0,0,0.1)",
-                height: "20em",
+                height: "15em",
                 width: "auto",
               }}
             />
@@ -69,7 +80,7 @@ export default function AboutSection() {
               alt="Oss bak Dus!"
               style={{
                 boxShadow: "0 4px 10px rgba(0,0,0,0.1)",
-                height: "20em",
+                height: "15em",
                 width: "auto",
               }}
             />
@@ -79,7 +90,7 @@ export default function AboutSection() {
               alt="Oss bak Dus!"
               style={{
                 boxShadow: "0 4px 10px rgba(0,0,0,0.1)",
-                height: "20em",
+                height: "15em",
                 width: "auto",
               }}
             />
