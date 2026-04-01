b2085a9fb5eae1e6a7c1d82420c291fdae7742fb
Philip Drivenes <philipdrivenes@gmail.com>
2026-04-01 09:13:31 +0200

    utseeende tjenester og om oss

--- app/(public)/tjenester/page.tsx
+++ app/(public)/tjenester/page.tsx
@@ -1,4 +1,5 @@
 import { getServices, getSiteContent } from "@/lib/content-db";
+import Image from "next/image";
 
 export default async function TjenesterPage() {
   const [services, introText] = await Promise.all([
@@ -7,82 +8,108 @@ export default async function TjenesterPage() {
   ]);
 
   return (
-    <div style={{ paddingTop: '60px', backgroundColor: '#f7f4f0', minHeight: '100vh' }}>
+    <div
+      style={{
+        backgroundColor: "#f7f4f0",
+        minHeight: "110vh",
+      }}
+    >
       <div
         style={{
-          marginTop: '15%',
-          paddingLeft: '7%',
-          paddingRight: '7%',
-          width: '85%',
-          display: 'flex',
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          alignContent: 'flex-start',
-          boxSizing: 'border-box',
+          paddingTop: "5%",
+          paddingLeft: "7%",
+          paddingRight: "7%",
+          width: "85%",
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignContent: "flex-start",
+          boxSizing: "border-box",
         }}
       >
         <p
           className="font-classico"
           style={{
             fontWeight: 200,
-            lineHeight: '1.4',
-            color: '#000000',
-            fontSize: 'clamp(10px, 2.5vw, 30px)',
-            textAlign: 'left',
-            width: '55%',
+            lineHeight: "1.4",
+            color: "#000000",
+            fontSize: "clamp(10px, 2.5vw, 30px)",
+            textAlign: "left",
+            width: "55%",
           }}
         >
           {introText}
         </p>
+        <Image
+          src="/img/byplanlegging.jpg"
+          alt="Byplanlegging illustrasjon"
+          width={400}
+          height={300}
+          style={{
+            width: "30%",
+
+            objectFit: "cover",
+          }}
+        />
       </div>
       <div
         className="font-classico"
         style={{
-          paddingLeft: '12%',
-          paddingRight: '12%',
-          marginTop: '10%',
-          marginBottom: '10%',
-          display: 'flex',
-          flexDirection: 'column',
-          gap: '0',
+          paddingLeft: "12%",
+          paddingRight: "12%",
+          marginTop: "5%",
+          marginBottom: "5%",
+          display: "flex",
+          flexDirection: "column",
+          gap: "0",
         }}
       >
         {services.map((service, index) => (
           <div
             key={service.id}
             style={{
-              position: 'relative',
-              width: '100%',
-              display: 'flex',
-              flexDirection: index % 2 !== 0 ? 'row-reverse' : 'row',
-              justifyContent: 'space-between',
-              alignItems: 'flex-start',
-              minHeight: '500px',
-              marginBottom: '10%',
-              borderTop: '1px solid #666669',
-              boxSizing: 'border-box',
-              paddingTop: '3%',
+              position: "relative",
+              width: "100%",
+              display: "flex",
+              flexDirection: index % 2 !== 0 ? "row-reverse" : "row",
+              justifyContent: "space-between",
+              alignItems: "flex-start",
+
+              marginBottom: "5%",
+              borderTop: "1px solid #666669",
+              boxSizing: "border-box",
+              paddingTop: "3%",
               opacity: 1,
             }}
           >
             <div
               style={{
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'center',
-                alignItems: 'flex-start',
-                width: '40%',
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "flex-start",
+                width: "40%",
               }}
             >
               <h2
                 className="font-classico"
-                style={{ fontSize: '1.1vw', textAlign: 'left', color: 'black', fontWeight: 400 }}
+                style={{
+                  fontSize: "1.1vw",
+                  textAlign: "left",
+                  color: "black",
+                  fontWeight: 400,
+                }}
               >
                 {service.title}
               </h2>
               <p
                 className="font-classico"
-                style={{ color: '#737373', fontWeight: 300, fontSize: '0.9rem', lineHeight: '1.8' }}
+                style={{
+                  color: "#737373",
+                  fontWeight: 300,
+                  fontSize: "0.9rem",
+                  lineHeight: "1.8",
+                }}
               >
                 {service.description}
               </p>
@@ -92,10 +119,10 @@ export default async function TjenesterPage() {
               src={service.image}
               alt={service.title}
               style={{
-                display: 'block',
-                height: 'auto',
-                maxWidth: '40%',
-                transition: '1.0s ease',
+                display: "block",
+                height: "auto",
+                maxWidth: "40%",
+                transition: "1.0s ease",
               }}
             />
           </div>
